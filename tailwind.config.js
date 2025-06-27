f1e5034aa3493c20024dcc5480f66109a559ffdb
Victor <vbivol394@gmail.com>
2025-06-27 22:58:11 +0300

    rezolvat problema

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}"
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
