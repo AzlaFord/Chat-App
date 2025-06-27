a77347aad43aac4560644e4ef93fdff2c8eac7d1
Victor <vbivol394@gmail.com>
2025-06-27 22:50:02 +0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded min-h-[100px] min-w-[200px]">
+      <h1 className="text-3xl font-bold text-red-500">Salut, Tailwind!</h1>
+      <p>Acum ai stil cu Tailwind în Next.js</p>
+    </div>
+  );
+}
