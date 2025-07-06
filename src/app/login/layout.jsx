8753f629a6d7f67d510eef728b6c78de63bfbf46
FORD <vbivol394@gmail.com>
2025-07-06 22:52:11 +0300

    redirect din login  pagina principala daca userul e deja logat

--- src/app/login/layout.jsx
+++ src/app/login/layout.jsx
@@ -1,24 +1,23 @@
 import { redirect } from 'next/navigation'
 import { cookies } from 'next/headers'
 import jwt from "jsonwebtoken"
-
 export default async function HomeLayout({ children }) {
   const cookie = await cookies()
   const token = cookie.get("token")?.value
-  if (!token) {
-    redirect('/home')
-  }
 
-  let payload
-  try {
-    payload = jwt.verify(token, process.env.JWT_SECRET)
-  } catch (err) {
-    redirect('/home')
-  }
-
-  if (!payload.role || payload.role !== "admin") {
+  if (token) {
     redirect('/home')
   }
   
-  return <>{children}</>
+  try{
+    const payload = jwt.verify(token,process.env.JWT_SECRET)
+    redirect("home")
+  }catch{
+    
+  }
+
+  return (<>
+    {children}
+  </>
+  )
 }
