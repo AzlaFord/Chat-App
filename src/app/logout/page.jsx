14a53c64631b498de320de1ff438d9c77a5fec94
FORD <vbivol394@gmail.com>
2025-07-07 12:48:17 +0300

    modificat logout page rezolvarea problema nu se stergea tokenul

--- src/app/logout/page.jsx
+++ src/app/logout/page.jsx
@@ -1,16 +1,10 @@
 'use client'
-
-import { useRouter } from 'next/navigation'
-
+import { redirect } from 'next/navigation'
 
 export default function logout(){
-    const router = useRouter()
 
-    async function handleLogout(){
-       await fetch("http://localhost:3000/api/logout",{
-            method:'POST',
-        })
-        router.push('/login')
+    function  handleLogout(){
+        redirect("/api/logout")
     }
 
     return(<>
