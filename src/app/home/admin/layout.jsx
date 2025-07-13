261141df1c231e93ed2f1a834e67c5384356f90f
Victor <vbivol394@gmail.com>
2025-07-13 19:40:08 +0300

    modificat structura

--- src/app/home/admin/layout.jsx
+++ src/app/home/admin/layout.jsx
@@ -1,14 +1,16 @@
 import { redirect } from 'next/navigation'
 import { cookies } from 'next/headers'
 import jwt from "jsonwebtoken"
+import Dashboard from './dashboard'
 
 export default async function HomeLayout({ children }) {
-  const cookie = await cookies()
-  const token = cookie.get("token")?.value
+  const cookieStore = cookies()
+  const token = cookieStore.get("token")?.value
+
   if (!token) {
     redirect('/home')
   }
-  
+
   let payload
   try {
     payload = jwt.verify(token, process.env.JWT_SECRET)
@@ -16,9 +18,14 @@ export default async function HomeLayout({ children }) {
     redirect('/home')
   }
 
-  if (!payload.role || payload.role !== "admin") {
+  if (payload.role !== "admin") {
     redirect('/home')
   }
-  
-  return <>{children}</>
+
+  return (
+    <>
+      <Dashboard />
+      {children}
+    </>
+  )
 }
